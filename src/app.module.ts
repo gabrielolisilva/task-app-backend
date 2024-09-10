8d04fa4725fc78018cca4017785be722b75a1a48
Gabriel Silva <gabrielsilva2799@hotmail.com>
2024-09-10 09:17:35 -0300

    FEATURE: Add globally prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamsModule } from './teams/teams.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [TeamsModule],
+  imports: [TeamsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
